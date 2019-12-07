2dbc288e626e643adcbcef13d33679b676493b66
Colin White <colinwhite@Colins-Mac-mini.local>
2019-12-07 14:40:07 +0000

    Initial Commit

---
+++ LiftEasy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiftEasy
+//
+//  Created by Colin White on 07/12/2019.
+//  Copyright © 2019 Colin White. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
