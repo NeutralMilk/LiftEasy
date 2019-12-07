fb7e65779a952da60c73ac94752e9f34f962580b
Colin White <colinwhite@Colins-Mac-mini.local>
2019-12-07 15:59:21 +0000

    Created lift class

---
+++ LiftEasy/Lifts.swift
@@ -0,0 +1,24 @@
+//
+//  Lifts.swift
+//  LiftEasy
+//
+//  Created by Colin White on 07/12/2019.
+//  Copyright © 2019 Colin White. All rights reserved.
+//
+
+import Foundation
+
+class Lifts
+{
+    var name = ""
+    var orm = 0.0
+    var ninety = 0.0
+    var eighty = 0.0
+    var seventy = 0.0
+    var fifty = 0.0
+    
+    func equalise(percent: Int, value: Float)
+    {
+        
+    }
+}
