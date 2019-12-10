f837628a05e76587f29deca3cd99ef173bab611d
Colin White <colinwhite@Colins-Mac-mini.local>
2019-12-10 16:46:11 +0000

    Completed autoequalise function for orm

--- LiftEasy/LiftViewController.swift
+++ LiftEasy/LiftViewController.swift
@@ -29,7 +29,7 @@ class LiftViewController: UIViewController
         addLiftButton.layer.cornerRadius = 5
         
         //Looks for single or multiple taps.
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
 
         view.addGestureRecognizer(tap)
     }
@@ -40,23 +40,62 @@ class LiftViewController: UIViewController
         view.endEditing(true)
     }
     
+    @IBAction func autoFill(_ sender: Any)
+    {
+        let newLift = Lifts()
+        
+        
+        if let orm = Double(orpTextField.text!)
+        {
+            newLift.equalise(percent: 1, value: orm)
+            
+            ninetyTextField.text = "\(newLift.weights[1])";
+            eightyTextField.text = "\(newLift.weights[2])";
+            seventyTextField.text = "\(newLift.weights[3])";
+            sixtyTextField.text = "\(newLift.weights[4])";
+            fiftyTextField.text = "\(newLift.weights[5])";
+        }
+        
+    }
+    
     @IBAction func addLift(_ sender: Any)
     {
         //create a new instance of a lift to add to the array
         let newLift = Lifts()
         
-        //get the name from the text field and add it to the array, then go back to the previous screen.
-        if let name = liftTextField.text, let orm = Double(orpTextField.text!)
+        //get the name from the text field and add it to the array, then go back to the previous screen. Make sure all fields are filled
+        if let name = liftTextField.text
         {
-            newLift.name = name
-            newLift.orm  = orm
-            newLift.equalise(percent: 1, value: orm)
-            print(newLift.ninety, " This is the newLift")
+            if let orm = Double(orpTextField.text!),
+                let ninety = Double(ninetyTextField.text!),
+                let eighty = Double(eightyTextField.text!),
+                let seventy = Double(seventyTextField.text!),
+                let sixty = Double(sixtyTextField.text!),
+                let fifty = Double(fiftyTextField.text!)
+            {
+                newLift.name = name
+                newLift.weights[0] = orm
+                newLift.weights[1] = ninety
+                newLift.weights[2] = eighty
+                newLift.weights[3] = seventy
+                newLift.weights[4] = sixty
+                newLift.weights[5] = fifty
                 
-            liftsTableVC?.lifts.append(newLift)
-            liftsTableVC?.tableView.reloadData()
-            navigationController?.popViewController(animated: true)
+                newLift.equalise(percent: 1, value: orm)
+                //print(newLift.weights[1], " This is the newLift")
+                    
+                liftsTableVC?.lifts.append(newLift)
+                liftsTableVC?.tableView.reloadData()
+                navigationController?.popViewController(animated: true)
+            }
+            else
+            {
+                let alert = UIAlertController(title: "Error", message: "Please fill all boxes", preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
         }
+            
     }
 
 }
--- LiftEasy/Lifts.swift
+++ LiftEasy/Lifts.swift
@@ -11,25 +11,56 @@ import Foundation
 class Lifts
 {
     var name = ""
-    var orm = 0.0
+    /*var orm = 0.0
     var ninety = 0.0
     var eighty = 0.0
     var seventy = 0.0
     var sixty = 0.0
-    var fifty = 0.0
+    var fifty = 0.0*/
+    
+    var weights =
+    [
+        0.0, //orm
+        0.0, //ninety
+        0.0, //eighty
+        0.0, //seventy
+        0.0, //sixty
+        0.0  //fifty
+    ]
     
     func equalise(percent: Int, value: Double)
     {
         let percent = percent
         let orm = value
+        
         if(percent == 1)
         {
-            ninety = orm * 0.9
-            eighty = orm * 0.8
-            seventy = orm * 0.7
-            sixty = orm * 0.6
-            fifty = orm * 0.5
+            weights[1] = orm * 0.9
+            weights[2] = orm * 0.8
+            weights[3] = orm * 0.7
+            weights[4] = orm * 0.6
+            weights[5] = orm * 0.5
         }
         
+        for i in 0...5
+        {
+            weights[i] = rounding(value: weights[i])
+            print(weights[i], " Value is")
+        }
+        
+    }
+    
+    
+    //round to the nearest 2.5kg (this is the smallest weight increment in gyms
+    func rounding(value: Double) -> Double
+    {
+        var value = value
+        
+        //round to nearest 2.5
+        value = value / 2.5
+        value.round()
+        value = value * 2.5
+        
+        return value
     }
 }
