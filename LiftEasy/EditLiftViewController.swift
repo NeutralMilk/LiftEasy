1768d0e0e6bb5391b89e20212948f1153af639b9
Colin White <colinwhite@Colins-Mac-mini.local>
2019-12-13 19:15:59 +0000

    Added ability to edit lifts

--- LiftEasy/EditLiftViewController.swift
+++ LiftEasy/EditLiftViewController.swift
@@ -43,5 +43,56 @@ class EditLiftViewController: UIViewController {
         fiftyTextField.text = "\(newLift.weights[5])"
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func updateAuto(_ sender: Any)
+    {
+        if let orm = Double(ormTextField.text!)
+        {
+            newLift.equalise(value: orm)
+            
+            ormTextField.text = "\(newLift.weights[0])"
+            ninetyTextField.text = "\(newLift.weights[1])"
+            eightyTextField.text = "\(newLift.weights[2])"
+            seventyTextField.text = "\(newLift.weights[3])"
+            sixtyTextField.text = "\(newLift.weights[4])"
+            fiftyTextField.text = "\(newLift.weights[5])"
+        }
+    }
+    
+    @IBAction func updateLift(_ sender: Any)
+    {
+        //get the name from the text field and add it to the array, then go back to the previous screen. Make sure all fields are filled
+        if let name = liftTextField.text, !liftTextField.text!.isEmpty,
+            let orm = Double(ormTextField.text!),
+            let ninety = Double(ninetyTextField.text!),
+            let eighty = Double(eightyTextField.text!),
+            let seventy = Double(seventyTextField.text!),
+            let sixty = Double(sixtyTextField.text!),
+            let fifty = Double(fiftyTextField.text!)
+        {
+            newLift.name = name
+            newLift.weights[0] = orm
+            newLift.weights[1] = ninety
+            newLift.weights[2] = eighty
+            newLift.weights[3] = seventy
+            newLift.weights[4] = sixty
+            newLift.weights[5] = fifty
+            
+            newLift.equalise(value: orm)
+            //print(newLift.weights[1], " This is the newLift")
+                
+            liftsTableVC?.lifts.append(newLift)
+            liftsTableVC?.tableView.reloadData()
+            navigationController?.popViewController(animated: true)
+        }
+        else
+        {
+            let alert = UIAlertController(title: "Error", message: "Please fill all boxes", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
 
 }
