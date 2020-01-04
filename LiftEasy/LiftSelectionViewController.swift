72a4876c7efcb2ceba8e11060b3651aacd44f98c
Colin White <colinwhite@Colins-Mac-mini.local>
2020-01-04 21:36:07 +0000

    added borer to buttons and segues

---
+++ LiftEasy/LiftSelectionViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LiftSelectionViewController.swift
+//  LiftEasy
+//
+//  Created by Colin White on 04/01/2020.
+//  Copyright © 2020 Colin White. All rights reserved.
+//
+
+import UIKit
+
+class LiftSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard()
+    {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- LiftEasy/ProgramViewController.swift
+++ LiftEasy/ProgramViewController.swift
@@ -10,23 +10,52 @@ import UIKit
 
 class ProgramViewController: UIViewController {
 
+    @IBOutlet weak var addProgramButton: UIButton!
+    @IBOutlet weak var mondayButton: UIButton!
+    @IBOutlet weak var tuesdayButton: UIButton!
+    @IBOutlet weak var wednesdayButton: UIButton!
+    @IBOutlet weak var thursdayButton: UIButton!
+    @IBOutlet weak var fridayButton: UIButton!
+    @IBOutlet weak var saturdayButton: UIButton!
+    @IBOutlet weak var sundayButton: UIButton!
+    
+    
     var programsTableVC : ProgramsTableViewController? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print("we got this far")
+        
+        addProgramButton.layer.cornerRadius = 5
+        
+        mondayButton.layer.cornerRadius = 5
+        mondayButton.layer.borderWidth = 1
+        mondayButton.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        tuesdayButton.layer.cornerRadius = 5
+        tuesdayButton.layer.borderWidth = 1
+        tuesdayButton.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        wednesdayButton.layer.cornerRadius = 5
+        wednesdayButton.layer.borderWidth = 1
+        wednesdayButton.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        thursdayButton.layer.cornerRadius = 5
+        thursdayButton.layer.borderWidth = 1
+        thursdayButton.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        fridayButton.layer.cornerRadius = 5
+        fridayButton.layer.borderWidth = 1
+        fridayButton.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        saturdayButton.layer.cornerRadius = 5
+        saturdayButton.layer.borderWidth = 1
+        saturdayButton.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        sundayButton.layer.cornerRadius = 5
+        sundayButton.layer.borderWidth = 1
+        sundayButton.layer.borderColor = UIColor.systemBlue.cgColor
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
